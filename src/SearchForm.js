cc9327c08f3d3c8a7c974946257787b7b806f8a2
emily-ecms <80700679+emily-ecms@users.noreply.github.com>
2022-05-23 21:39:52 +0100

    Added useState and functions to set word searched

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SearchForm() {
+    const [word, setWord] = useState("");
+
+    function searchWord(event) {
+        event.preventDefault();
+        alert(`You searched for "${word}"`);
+    }
+
+    function saveWord(event) {
+        setWord(event.target.value);
+    }
+
     return (
-        <form className="SearchForm">
-            <input type="search" placeholder="Search for a word"></input>
+        <form className="SearchForm" onSubmit={searchWord}>
+            <input type="search" placeholder="Search for a word" onChange={saveWord}></input>
             <button className="btn btn-primary rounded">Search</button>
         </form>
     )
