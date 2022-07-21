63991cf5fe027e417e4297ec17dbca19d52d729f
emily-ecms <80700679+emily-ecms@users.noreply.github.com>
2022-07-21 16:48:37 +0100

    Added Photos component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,11 +3,13 @@ import "./Dictionary.css";
 import "./App.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
     let [word, setWord] = useState(props.defaultWord);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
     function getResults(response) {
         //console.log(response.data[0]);
@@ -15,10 +17,20 @@ export default function Dictionary(props) {
         setResults(response.data[0]);
     }
 
+    function handlePexelsResponse(response) {
+        console.log(`HERE'S THE PICS OF: ${word}`);
+        console.log(response.data.photos);
+        setPhotos(response.data.photos);
+    }
+
     function search() {
          // documentation: https://dictionaryapi.dev/
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
         axios.get(apiUrl).then(getResults);
+        let pexelsApiKey = "563492ad6f917000010000010b067f8193504972899c4e6ad2c2afc6";
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${word}&per_page=9`;
+        let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     }
 
     function handleSubmit(event) {
@@ -50,6 +62,8 @@ export default function Dictionary(props) {
         </section>
         
          <Results results={results} />
+
+         <Photos photos={photos} />
          
          </div>
     )
